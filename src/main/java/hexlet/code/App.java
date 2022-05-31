56c958a7a543cd89c381827d8799a000bb77b92c
dmartynov <denis.martynov.93@mail.ru>
2022-05-31 14:17:57 +0300

    add greetings

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,6 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] arg) {
-        System.out.println("Welcome to the Brain Games!");
+        Cli.greetings();
     }
 }
